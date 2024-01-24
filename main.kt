9089f752b9923c8f177abbb66e8733932f5df902
Quiet Soto <bylina.alexa2005@gmail.com>
2024-01-24 20:20:23 +0200

    Printing message 'hello world' 10 times

---
+++ main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    repeat(10) {
+        println("Hello, World!")
+    }
+    vivaMechMat() // Используем правильное название функции
+}
+
+fun vivaMechMat() {
+    println("Viva Mech-mat faculty!!!")
+}
