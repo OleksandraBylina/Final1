258922dddbb0da056a3945697e329be84baf5be4
Quiet Soto <bylina.alexa2005@gmail.com>
2024-02-09 11:52:34 +0200

    hydro

--- utils.py
+++ utils.py
@@ -15,4 +15,27 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
+<<<<<<< Updated upstream
+=======
+def pyatirka(n):
+    if n%5!=0:
+        return False
+    while n%5==0:
+        n=n/5
+    if n%5!=0 and n!=1:
+        return False
+    else:
+        return True
+
+def dva(n):
+    if n%2!=0:
+        return False
+    while n%2==0:
+        n=n/2
+    if n%2!=0 and n!=1:
+        return False
+    else:
+        return True
+
+>>>>>>> Stashed changes
 
